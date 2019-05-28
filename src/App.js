9c62b7e560abf26e2273c96cd9ed31bdc8be0ff9
seungyeon <ream150346@gmail.com>
2019-05-28 12:40:49 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
 import React, {Component} from 'react';
 import './App.css';
 import Student from './components/Student'
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
 
-const student = {
+const styles = theme =>({
+  root:{
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
+
+const students = [
+{
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
   'name': '홍길동',
   'birthday': '961112',
   'gender': '남자',
   'job': '대학생'
-}
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '홍길순',
+  'birthday': '951022',
+  'gender': '여자',
+  'job': '프로그래머'
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '송승연',
+  'birthday': '961112',
+  'gender': '남자',
+  'job': '대학생'
+},
+]
 
 class App extends Component{
   render(){
     return (
-      <Student
-        name={student.name}
-        birthday={student.birthday}
-        gender={student.gender}
-        job={student.job}
-      />
+      <Paper className={styles.root}>
+        <Table className={styles.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {students.map(c => { return( <Student key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>);})}
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
